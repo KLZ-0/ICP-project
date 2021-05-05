21725c0999d5b2ff1b3685beadfafb5a5fd2de25
KLZ-0 <adrian@kalazi.com>
2021-05-05 23:00:40 +0200

    Populate explorer TreeView with some sample data

--- src/gui/explorer.cpp
+++ src/gui/explorer.cpp
@@ -3,6 +3,21 @@
  */
 
 #include "explorer.hpp"
+
+#include <iostream>
 Explorer::Explorer() {
 	ui.setupUi(this);
+
+	auto item = new QTreeWidgetItem;
+	item->setText(0, "top level item");
+
+	auto subItem = new QTreeWidgetItem;
+	subItem->setText(0, "lower level item");
+
+	auto subsubItem = new QTreeWidgetItem;
+	subsubItem->setText(0, "even lower level item");
+
+	ui.treeWidget->addTopLevelItem(item);
+	item->addChild(subItem);
+	subItem->addChild(subsubItem);
 }
