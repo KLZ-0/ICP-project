84bc209b49724b6c2338cd5b7b49dc02d40e4fe1
KLZ-0 <adrian@kalazi.com>
2021-05-05 22:25:50 +0200

    Add multi-level UI

---
+++ src/gui/explorer.cpp
@@ -0,0 +1,8 @@
+/**
+ * @author Adrián Kálazi (xkalaz00)
+ */
+
+#include "explorer.hpp"
+Explorer::Explorer() {
+	ui.setupUi(this);
+}
---
+++ src/gui/explorer.hpp
@@ -0,0 +1,24 @@
+/**
+ * @author Adrián Kálazi (xkalaz00)
+ */
+
+#ifndef MQTT_EXPLORER_EXPLORER_HPP
+#define MQTT_EXPLORER_EXPLORER_HPP
+
+
+#include <QWidget>
+
+#include "ui_explorer.h"
+
+class Explorer : public QWidget
+{
+	Q_OBJECT
+public:
+	Explorer();
+
+private:
+	Ui::Explorer ui;
+};
+
+
+#endif //MQTT_EXPLORER_EXPLORER_HPP
