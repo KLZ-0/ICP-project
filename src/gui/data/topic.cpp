f29bd6ae1d180d5ddcc97632c352d8eb41a2f1db
KLZ-0 <adrian@kalazi.com>
2021-05-08 18:22:40 +0200

    Document datamodel

--- src/gui/data/topic.cpp
+++ src/gui/data/topic.cpp
@@ -6,12 +6,23 @@
 
 #include <QDebug>
 
+/**
+ * @brief Initializes a new topic
+ * @param name topic name
+ * @param messageLimit topic message limit (max history)
+ * @param parentTopic parent topic of the new topic, null if root topic
+ */
 Topic::Topic(QString &name, int messageLimit, Topic *parentTopic) {
 	this->name = name;
 	limit = messageLimit;
 	parent = parentTopic;
 }
 
+/**
+ * @brief Adds a new payload (message) to this topic
+ * Also removes the oldest payload if the received message count exceeds the limit
+ * @param new_payload
+ */
 void Topic::addPayload(const QString &new_payload) {
 	lastTimestamp = std::time(nullptr);
 
@@ -24,6 +35,11 @@ void Topic::addPayload(const QString &new_payload) {
 	emit changed();
 }
 
+/**
+ * @brief Acquires a payload (message)
+ * @param index index of the payload, 0 being the most recent
+ * @return payload as a QString, an empty string on non-existent index
+ */
 QString Topic::getPayload(int index) {
 	if (index < payloads.size()) {
 		return payloads.at(index);
@@ -32,10 +48,18 @@ QString Topic::getPayload(int index) {
 	}
 }
 
+/**
+ * @brief Getter for the timestamp of the most recent message
+ * @return timestamp as std::time_t (long int)
+ */
 std::time_t Topic::getTimestamp() {
 	return lastTimestamp;
 }
 
+/**
+ * @brief Constructs a formatted string from the timestamp of the most recent message
+ * @return
+ */
 QString Topic::getTimestampString() {
 	if (lastTimestamp == 0) {
 		return "";
@@ -49,22 +73,44 @@ QString Topic::getTimestampString() {
 	return QString(mbstr);
 }
 
+/**
+ * @brief Count of all messages received in this topic (even if not stored due to limited history)
+ * @return message count
+ */
 int Topic::messageCount() const {
 	return count;
 }
 
+/**
+ * @brief Sets a new message limit (takes effect only on addPayload)
+ * @param messageLimit new message limit
+ */
 void Topic::setMessageLimit(int messageLimit) {
 	limit = messageLimit;
 }
 
+/**
+ * @brief Getter for name
+ * @return name
+ */
 QString Topic::getName() {
 	return name;
 }
 
+/**
+ * Getter for parent topic
+ * @return parent topic
+ */
 Topic *Topic::getParent() {
 	return parent;
 }
 
+/**
+ * @brief Constructs a string representing the fully qualified name using the parent topics
+ * This name can be used to uniquely identify topics
+ * e.g. a/b/c/d with d being this topic, the children of this topic are not considered
+ * @return QString with fully qualified name
+ */
 QString Topic::findFullyQualifiedTopic() {
 	QString topicString = this->getName();
 
@@ -77,6 +123,11 @@ QString Topic::findFullyQualifiedTopic() {
 	return topicString;
 }
 
+/**
+ * @brief Forces the timestamp to the new value
+ * Used mainly when loading the dashboard from a file
+ * @param newTimeStamp new timestamp as std::time_t (long int)
+ */
 void Topic::setTimestamp(std::time_t newTimeStamp) {
 	lastTimestamp = newTimeStamp;
 }
