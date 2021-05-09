71ed37f338ff6d0198da5b7eb3f935896e2d8e56
Kevin Lackó <kevinlacko.kl@gmail.com>
2021-05-09 18:41:55 +0200

    Documentation for Core

--- src/core/client.hpp
+++ src/core/client.hpp
@@ -12,35 +12,74 @@
 
 namespace Core
 {
+	/**
+	 * @class Client
+	 * @brief Implements a client for connecting to and communicating with and mqtt server
+	 */
 	class Client : public QObject
 	{
 		Q_OBJECT
 	public:
 		Client() = delete;
+		/**
+		 * @brief Client Constructor
+		 * @param clientId
+		*/
 		Client(const std::string &clientId);
 
 	public slots:
+		/**
+		 * @brief	Connects to the default mqtt server: tcp://test.mosquitto.org:1883
+		*/
 		void Connect();
-
+		/**
+		 * @brief Disconnect from the server
+		*/
 		void Disconnect();
-
+		/**
+		 * @brief Subscirbe to a set of topics on the currently connected server
+		 * @param topics 
+		*/
 		void Subscribe(const QSet<QString> &topics);
-
+		/**
+		 * @brief Unsubscribe a set of topics from the currently connected server
+		 * @param topics 
+		*/
 		void Unsubscribe(const QSet<QString> &topics);
-
+		/**
+		 * @brief Publish a mqtt message to the currently connected server
+		 * @param msg 
+		*/
 		void Publish(mqtt::const_message_ptr msg);
 
 	signals:
+		/**
+		 * @brief Qt signal when we successfully connect to a server
+		*/
 		void Connected();
-
+		/**
+		 * @brief Qt signal when we loose connection from a server
+		 * @param cause 
+		*/
 		void ConnectionLost(const QString &cause);
-
+		/**
+		 * @brief Qt signal for incoming messages
+		 * @param msg 
+		*/
 		void MessageArrived(mqtt::const_message_ptr msg);
-
+		/**
+		 * @brief Qt signal when we we failed to (re)connect after 5 automatic tries
+		*/
 		void ServerUnreachable();
-
+		/**
+		 * @brief Qt signal for a successful mqtt action - info in token
+		 * @param tok 
+		*/
 		void ActionSucceeded(const mqtt::token &tok);
-
+		/**
+		 * @brief Qt for a failed mqtt action - info in token
+		 * @param tok 
+		*/
 		void ActionFailed(const mqtt::token &tok);
 
 	private:
