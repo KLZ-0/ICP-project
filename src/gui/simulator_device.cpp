d63839ddcc2da3c99a13d3a27a2d8f408bbc5fc2
Kevin Lackó <kevinlacko.kl@gmail.com>
2021-05-10 19:15:45 +0200

    forgot to add simulator_device source :facepalm:

---
+++ src/gui/simulator_device.cpp
@@ -0,0 +1,75 @@
+/**
+ * @author Kevin Lackó (xlacko08)
+ */
+
+#include "simulator_device.hpp"
+
+#include <qjsonarray.h>
+#include <qjsonobject.h>
+
+SimulatorDevice::SimulatorDevice(const QJsonObject &deviceConfigJson) {
+	setDeviceConfigfromJson(deviceConfigJson);
+}
+
+SimulatorDevice::SimulatorDevice(DeviceConfig deviceConfig) : deviceConfig(deviceConfig) {
+}
+
+void SimulatorDevice::start() {
+}
+
+void SimulatorDevice::stop() {
+}
+
+QJsonObject SimulatorDevice::toJson() const {
+	QJsonObject deviceJson;
+	deviceJson.insert("topic", deviceConfig.topic);
+	deviceJson.insert("deviceType", deviceConfig.deviceType);
+
+	if (deviceConfig.deviceType == "publisher") {
+		deviceJson.insert("interval_ms", deviceConfig.devicePublisher.interval_ms);
+		deviceJson.insert("dataType", deviceConfig.devicePublisher.dataType);
+
+		if (deviceConfig.devicePublisher.dataType == "string") {
+			deviceJson.insert("randomData", deviceConfig.devicePublisher.randomData);
+
+			QJsonArray dataArray;
+			for (const auto &str : deviceConfig.devicePublisher.data) {
+				dataArray.append(str);
+			}
+			deviceJson.insert("data", dataArray);
+
+		} else /* "binary" */ {
+			deviceJson.insert("filePath", deviceConfig.devicePublisher.filePath);
+		}
+
+	} else /* "receiver" */ {
+		deviceJson.insert("targetTopic", deviceConfig.deviceReciever.targetTopic);
+		deviceJson.insert("delay_ms", deviceConfig.deviceReciever.delay_ms);
+	}
+
+	return deviceJson;
+}
+
+void SimulatorDevice::setDeviceConfigfromJson(const QJsonObject &deviceConfigJson) {
+	const QJsonObject &json = deviceConfigJson;
+	deviceConfig.topic = json["topic"].toString();
+	deviceConfig.deviceType = json["deviceType"].toString();
+
+	if (deviceConfig.deviceType == "publisher") {
+		deviceConfig.devicePublisher.interval_ms = json["interval_ms"].toInt();
+		deviceConfig.devicePublisher.dataType = json["dataType"].toString();
+
+		if (deviceConfig.devicePublisher.dataType == "string") {
+			QJsonArray dataArray = json["data"].toArray();
+			for (const auto &value : dataArray) {
+				deviceConfig.devicePublisher.data.append(value.toString());
+			}
+		} else /* "binary" */ {
+			deviceConfig.devicePublisher.filePath = json["filePath"].toString();
+		}
+
+	} else /* "receiver" */ {
+		deviceConfig.deviceReciever.targetTopic = json["targetTopic"].toString();
+		deviceConfig.deviceReciever.delay_ms = json["delay_ms"].toInt();
+	}
+}
---
+++ src/gui/simulator_device.hpp
@@ -0,0 +1,77 @@
+/**
+ * @author Kevin Lackó (xlacko08)
+ */
+#pragma once
+
+#include <qobject.h>
+#include <qtimer.h>
+
+class SimulatorDevice : public QObject
+{
+	Q_OBJECT
+public:
+	/**
+	 * @brief Device which sends data
+	*/
+	struct DevicePublisher
+	{
+		// periodicity for sending the data
+		int interval_ms;
+		// "string" or "binary"
+		QString dataType;
+		// choose data randomly vs consecutively
+		bool randomData;
+		// string data to be sent
+		QVector<QString> data;
+		// binary file to be sent
+		QString filePath;
+	};
+
+	struct DeviceReceiver
+	{
+		// the topic to which we want to publish the update
+		QString targetTopic;
+		// delay after which to publish the update
+		int delay_ms;
+	};
+
+	/**
+	 * @brief configuration data of a simulated device
+	*/
+	struct DeviceConfig
+	{
+		// publish or recieve data from this topic
+		QString topic;
+		// select device type ("receiver"/"publisher")
+		QString deviceType;
+		// actual config for publisher
+		DevicePublisher devicePublisher;
+		// actual config for receiver
+		DeviceReceiver deviceReciever;
+	};
+
+public:
+	SimulatorDevice(const QJsonObject& deviceConfigJson);
+	SimulatorDevice(DeviceConfig deviceConfig);
+
+	void start();
+
+	void stop();
+
+	/**
+	 * @brief serialize device config to json
+	 * @return device config as QJsonObject
+	*/
+	QJsonObject toJson() const;
+
+private:
+	/**
+	 * @brief parse json object
+	 * @param  
+	*/
+	void setDeviceConfigfromJson(const QJsonObject& deviceConfigJson);
+
+private:
+	QTimer timer;
+	DeviceConfig deviceConfig;
+};
